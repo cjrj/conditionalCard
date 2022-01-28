d6a38b720b952666c366211219c2b71f3c3b7bf4
cjrj <crivasjayo@gmail.com>
2022-01-28 14:58:21 -0400

    :cactus: Entrega de ejercicio

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import "../style/index.css";
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
+ *  
     {
         includeCover: true, // if includeCover is true the algorithm should
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
@@ -33,24 +33,24 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name == null ? "" : variables.name} ${
-    variables.lastname == null ? "" : variables.lastname
+          <h1>${variables.name == null ? "Name" : variables.name} ${
+    variables.lastname == null ? "Lastname" : variables.lastname
   }</h1>
-          <h2>${variables.role}</h2> 
-          <h3>${variables.city}</h3>
-          <h3>${variables.country}</h3>
+          <h2>${variables.role == null ? "Role" : variables.role}</h2> 
+          <h3>${variables.city == null ? "City" : variables.city}</h3>
+          <h3>${variables.country == null ? "Country" : variables.country}</h3> 
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${
-              variables.twitter
+              variables.twitter == null ? "" : variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${
-              variables.github
+              variables.github == null ? "" : variables.github
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${
-              variables.linkedin
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin == null ? "" : variables.linkedin
             }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${
-              variables.instagram
+              variables.instagram == null ? "" : variables.instagram
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
@@ -69,10 +69,10 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: null,
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
